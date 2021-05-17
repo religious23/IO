1f506f22dc95cb3ecf695a095e237df4aa5b9b5e
wangwen <1035354510@qq.com>
2021-05-17 13:40:13 +0800

    IO

--- src/main/java/io/SocketIo.java
+++ src/main/java/io/SocketIo.java
@@ -1,5 +1,9 @@
 package io;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author 王文
  * @version : V1.0
@@ -8,4 +12,34 @@ package io;
  * @date 2021-05-17
  */
 public class SocketIo {
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(9090, 20);
+        System.out.println("step1: new ServerSocket(9090)");
+
+        while (true) {
+            //阻塞1
+            Socket client = server.accept();
+            System.out.println("step2:client\t" + client.getPort());
+
+            new Thread(() -> {
+                InputStream in = null;
+                try {
+                    in = client.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    while (true) {
+                        String dataline = reader.readLine();//阻塞2
+                        if (null != dataline) {
+                            System.out.println(dataline);
+                        }else{
+                            client.close();
+                            break;
+                        }
+                    }
+                    System.out.println("客户端断开");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
 }
